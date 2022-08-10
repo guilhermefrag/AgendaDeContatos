c5bab062ab22254fd479c8451c17e24adfcd61a9
Guilherme Fragnani Bez Fontana <70298434+guilhermefrag@users.noreply.github.com>
2022-08-09 21:27:44 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,19 +1,3 @@
-/*
-
-1. Criar a classe Contato com os seguintes atributos:
-    a. Nome;
-    b. Email;
-    c. Telefone;
-2. Definir uma classe (Pode ser chamada de Agenda) para gerenciar uma agenda telefônica capaz de armazenar 100 contatos.
-3. Implementar os seguintes métodos:
-    a. adicionar() : recebe um objeto e adiciona no array. Este método deverá
-    retornar um boolean indicando se conseguiu ou não fazer a adição do
-    contato.
-    b. exibirContatos() : Imprimi uma String com os dados de todos os objetos
-    contatos.
-    c. remover() : remove da lista um contato pelo nome
- */
-
 import Models.*;
 
 public class App {
